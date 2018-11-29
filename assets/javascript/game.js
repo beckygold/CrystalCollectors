b241dc617b861d80be31077e870d25c24b6d907e
Becky Gold <43392694+beckygold@users.noreply.github.com>
2018-11-28 22:49:44 -0700

    edited index.html and game.js. added image files for crystal buttons.

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// create variables for all elements
+
+var wins = 0;
+var losses = 0;
+var totalScore = " ";
+var randomNumber = " ";
+var crystalBlue = " ";
+var crystalGreen = " ";
+var crystalRed = " ";
+var crystalYellow = " ";
+
+// the computer picks a random number between 19 - 120
+
+// each crystal is set to a random hidden value between 1 - 12
+
+// when crystal is clicked, add the hidden value to the player's total score
+
+// player wins if total score = random number
+
+// player loses if total score > random number
+
+// game restarts whenever player wins / loses. page does not refresh.
