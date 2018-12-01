2da837326cf34b803ce489ee977f53a3a419d0ca
Becky Gold <43392694+beckygold@users.noreply.github.com>
2018-11-30 23:20:56 -0700

    modified game.js, style.css and index.html. added background image.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,23 @@
 var wins = 0;
 var losses = 0;
 var totalScore = " ";
-var randomNumber = " ";
+var targetNumber = " ";
 var crystalBlue = " ";
 var crystalGreen = " ";
 var crystalRed = " ";
 var crystalYellow = " ";
 
-var randomNumberText = document.getElementById("randomnumber-text");
+var targetNumberText = document.getElementById("targetnumber-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var totalScoreText = document.getElementById("totalscore-text");
 
 // the computer picks a random number between 19 - 120 and 
 function start() {
-    var randomNumber = Math.floor(Math.random() * (120 - 19) + 19);
-    console.log("Number: " + randomNumber);
-    randomNumberText.textContent = "Random Number: " + randomNumber;
+    var targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
+    console.log("Number: " + targetNumber);
+    // targetNumberText.textContent = "Target Number: " + targetNumber.value;
+    $("#targetNumberText").html(targetNumber);
     
 // each crystal is set to a random hidden value between 1 - 12
     var crystalBlue = Math.floor(Math.random() * (12 - 1) + 1);
@@ -34,7 +35,10 @@ function start() {
 start()
 
 // when crystal is clicked, add the hidden value to the player's total score
-
+$("#crystalBlue").click(function() {
+    totalScore.value += crystalBlue;
+    $('#totalScoreText').html(totalScore);
+});
 // player wins if total score = random number
 
 // player loses if total score > random number
